dc77ab1a9ab9b03a1fc4d80016c27628e594e07e
mdraushan <mdraushanji22@gmail.com>
2026-01-23 23:57:51 +0530

    php Opps

--- properties.php
+++ properties.php
@@ -7,6 +7,11 @@ class Properties
 
         echo $this->name;
     }
+    function updateName($name)
+    {
+        $this->name = $name;
+    }
 }
 $p1 = new Properties();
+$p1->updateName("Jilani");
 $p1->GetName();
