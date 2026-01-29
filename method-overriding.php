77830b58244c011d1880e727aa4471004c341529
mdraushan <mdraushanji22@gmail.com>
2026-01-29 18:54:17 +0530

    method-overriding

--- method-overriding.php
+++ method-overriding.php
@@ -11,3 +11,20 @@ class Teacher
         echo "my age is 24";
     }
 }
+class Student extends Teacher
+{
+    public $city = "Noida";
+    function age()
+    {
+        echo "my age is 40";
+    }
+}
+$t1 = new Teacher();
+$t1->age();
+echo "<br>";
+
+$st1 = new Student();
+$st1->age();
+echo "<br>";
+
+echo $st1->city;
