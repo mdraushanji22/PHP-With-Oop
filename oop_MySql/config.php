fdc40ab4f42c25d347962ed18cd3e8a1b0308c6e
mdraushan <mdraushanji22@gmail.com>
2026-01-31 22:51:08 +0530

    oop_mysql

--- oop_MySql/config.php
+++ oop_MySql/config.php
@@ -3,5 +3,10 @@ $host = "localhost";
 $username = "root";
 $password = null;
 
-$conn = new PDO("mysql:host=$host;dbname =college", $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+try {
+    // FIXED connection string
+    $conn = new PDO("mysql:host=$host;dbname=college", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
--- oop_MySql/student.php
+++ oop_MySql/student.php
@@ -1,15 +1,28 @@
+
 <?php
+include('./config.php');
 class Student
 {
     public $DBconn;
+
     function __construct($conn)
     {
-
         $this->DBconn = $conn;
     }
+
     function getData()
     {
-        $getStudent = $this->DBconn->prepare("select * from students");
+        $getStudent = $this->DBconn->prepare("SELECT * FROM students");
+        $getStudent->execute();
+        $result = $getStudent->fetchAll(PDO::FETCH_ASSOC);
+
+        echo "<pre>";
+        print_r($result);
+        echo "</pre>";
     }
 }
+
+
+// Create object
 $student = new Student($conn);
+$student->getData();
