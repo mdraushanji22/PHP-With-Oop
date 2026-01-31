7f9e176177b9b25bd6782c8789e44112d15b138f
mdraushan <mdraushanji22@gmail.com>
2026-01-31 23:30:46 +0530

    oop_mysql

--- oop_MySql/student.php
+++ oop_MySql/student.php
@@ -20,9 +20,22 @@ class Student
         print_r($result);
         echo "</pre>";
     }
+    function insertData()
+    {
+        $sqlQuery = 'insert into students (`id`, `name`, `course`,`batch`,`city`,`year`)
+         values (null,"Rahul","M.Tech","evening","New Delhi","2nd")';
+        $student = $this->DBconn->prepare($sqlQuery);
+        $result = $student->execute();
+        if ($result) {
+            echo "Insert Data successfull";
+        } else {
+            echo "Operation failled";
+        }
+    }
 }
 
 
 // Create object
 $student = new Student($conn);
 $student->getData();
+$student->insertData();
